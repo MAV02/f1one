d1758154fd17d0e7b06ec10657e46134ecc74231
MAV02 <m.vold@me.com>
2025-05-30 12:36:06 +0200

    fix 07

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
+// app/layout.tsx
+
 import './globals.css';
 import { ReactNode } from 'react';
 import { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'ONEF1',
@@ -9,8 +14,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="dark">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
